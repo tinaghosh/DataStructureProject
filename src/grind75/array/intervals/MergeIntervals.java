98bf9e4ea5f2c885c3d3d6d40b50d03bbb80caf3
tinaghosh <ghosh.tina90@gmail.com>
2026-01-09 06:54:05 -0800

    Binary Search

--- src/grind75/array/intervals/MergeIntervals.java
+++ src/grind75/array/intervals/MergeIntervals.java
@@ -1,4 +1,42 @@
 package grind75.array.intervals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MergeIntervals {
+
+    public int[][] mergeIntervals(int[][] intervals){
+
+        Arrays.sort(intervals, (e1, e2)->e1[0]-e2[0]);
+        System.out.println("Sorting based on intervals starting points");
+        Arrays.stream(intervals).forEach(e-> System.out.print("["+e[0]+"----"+e[1]+"]"));
+
+        List<int[]> listOutput = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        listOutput.add(currentInterval);
+
+        for(int i=1;i<intervals.length;i++){
+
+            currentInterval = listOutput.getLast();
+
+            int currentEnd = currentInterval[1];
+            int nextStart = intervals[i][0];
+            int nextEnd = intervals[i][1];
+
+            if(currentEnd>=nextStart){
+                currentInterval[1] = Math.max(currentEnd,nextEnd);
+            }else{
+                listOutput.add(intervals[i]);
+            }
+        }
+        return listOutput.toArray(int[][]::new);
+    }
+    public static void main(String[] args){
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        MergeIntervals obj = new MergeIntervals();
+        int[][] results = obj.mergeIntervals(intervals);
+        System.out.println();
+        Arrays.stream(results).forEach(e-> System.out.print("["+e[0]+"----"+e[1]+"]"));
+    }
 }
--- src/grind75/binary_search/BinarySearch.java
+++ src/grind75/binary_search/BinarySearch.java
@@ -1,4 +1,34 @@
 package grind75.binary_search;
 
 public class BinarySearch {
+
+    public int findElement(int[] nums, int target){
+
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+               right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int[] nums = {5};//{-1,0,3,5,9,12};
+        int target = 5; //9;//2;
+
+        BinarySearch obj = new BinarySearch();
+        int result = obj.findElement(nums,target);
+        if(result>=0) {
+            System.out.println(target + " exists in nums and its index is " + result);
+        }else{
+            System.out.println(target + " does not exist in nums so return -1 ");
+        }
+    }
 }
