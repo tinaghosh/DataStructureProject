70ea25046a68baf4a17cabc6c8d23dbf78149096
tinaghosh <ghosh.tina90@gmail.com>
2026-01-13 12:17:44 -0800

    Tree

--- src/grind75/binary_tree/MaximumDepthOfBinaryTree.java
+++ src/grind75/binary_tree/MaximumDepthOfBinaryTree.java
@@ -1,4 +1,102 @@
 package grind75.binary_tree;
 
+import com.sun.source.tree.Tree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class MaximumDepthOfBinaryTree {
+
+    //Recursive DFS
+    public int findMaxDepthOfBinaryTreeUsingRecursiveDFS(TreeNode root){
+
+        int depth;
+
+        if(root == null){
+            depth = 0;
+        }else{
+           depth = 1+ Math.max(findMaxDepthOfBinaryTreeUsingRecursiveDFS(root.leftNode),findMaxDepthOfBinaryTreeUsingRecursiveDFS(root.rightNode));
+        }
+      return depth;
+    }
+
+    //Iterative DFS
+    public int findMaxDepthOfBinaryTreeUsingIterativeDFS(TreeNode root){
+        int depth = 0;
+
+        if(root == null){
+            return depth;
+        }else {
+
+            Stack<Pair> stack = new Stack<>();
+            stack.push(new Pair(root,depth+1));
+
+            while(!stack.isEmpty()){
+                   Pair pairNode = stack.pop();
+                   depth = Math.max(depth, pairNode.depth);
+
+                   if(pairNode.node.leftNode!=null){
+                       stack.add(new Pair(pairNode.node.leftNode,pairNode.depth+1));
+                   }
+                   if(pairNode.node.rightNode!=null){
+                       stack.add(new Pair(pairNode.node.rightNode,pairNode.depth+1));
+                   }
+            }
+        }
+        return depth;
+    }
+
+    //BFS
+    public int findMaxDepthOfBinaryTreeUsingBFS(TreeNode root) {
+
+        int depth = 0;
+
+        if (root == null) {
+            return depth;
+        } else {
+
+            Deque<TreeNode> queue = new LinkedList();
+            queue.add(root);
+
+            while (!queue.isEmpty()) {
+                for (int i = 0; i < queue.size(); i++) {
+                    TreeNode node = queue.pop();
+                    if (node.leftNode != null) {
+                        queue.add(node.leftNode);
+                    }
+                    if (node.rightNode != null) {
+                        queue.add(node.rightNode);
+                    }
+                }
+                depth++;
+            }
+        }
+        return depth;
+    }
+
+
+public static void main(String[] args){
+
+    Object[] rootArray = {3,9,20,null,null,15,7};
+
+    TreeNode root = new TreeNode((int)rootArray[0]);
+    root.leftNode = new TreeNode((int)rootArray[1]);
+    root.rightNode = new TreeNode((int)rootArray[2]);
+    root.rightNode.leftNode = new TreeNode((int)rootArray[5]);
+    root.rightNode.rightNode = new TreeNode((int)rootArray[6]);
+
+    MaximumDepthOfBinaryTree obj = new MaximumDepthOfBinaryTree();
+
+    int result = obj.findMaxDepthOfBinaryTreeUsingRecursiveDFS(root);
+    System.out.println("Maximum Depth Of Binary Tree is using Recursive DFS : "+result);
+
+    int result2 = obj.findMaxDepthOfBinaryTreeUsingIterativeDFS(root);
+    System.out.println("Maximum Depth Of Binary Tree is using Iterative DFS : "+result2);
+
+    int result3 = obj.findMaxDepthOfBinaryTreeUsingBFS(root);
+    System.out.println("Maximum Depth Of Binary Tree is using BFS : "+result3);
+
+  }
 }
---
+++ src/grind75/binary_tree/TreeNode.java
@@ -0,0 +1,38 @@
+package grind75.binary_tree;
+
+import com.sun.source.tree.Tree;
+
+public class TreeNode {
+
+    int data;
+    TreeNode leftNode;
+    TreeNode rightNode;
+
+    TreeNode(){
+
+    }
+
+    TreeNode(int data){
+        this.data = data;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+
+    TreeNode(int data , TreeNode leftNode, TreeNode rightNode){
+       this.data = data;
+       this.leftNode = leftNode;
+       this.rightNode = rightNode;
+    }
+}
+
+class Pair {
+
+    TreeNode node ;
+    int depth;
+
+    Pair(TreeNode node, int depth){
+        this.node = node;
+        this.depth = depth;
+    }
+
+}
