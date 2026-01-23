f2ad75445b787e04692ae8012b965c2e3964cff6
tinaghosh <ghosh.tina90@gmail.com>
2026-01-23 10:30:14 -0800

    valid anagram

---
+++ src/grind75/string/hashmap/ValidAnagram.java
@@ -0,0 +1,83 @@
+package grind75.string.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+    public boolean checkValidAnagramWithoutExtraSpace(String s, String t){
+
+       if(s.length() !=t.length()){
+           return false;
+       }
+
+       int[] alphabet = new int[26];
+
+       for(Character c: s.toCharArray()){
+          int index = c - 'a';
+          alphabet[index]++;
+       }
+
+       for(Character c: t.toCharArray()){
+           int index = c-'a';
+           alphabet[index]--;
+           if(alphabet[index]<0){
+               return false;
+           }
+       }
+       return true;
+    }
+
+    public boolean checkValidAnagram(String t, String s){
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        Map<Character,Integer> table = new HashMap<>();
+
+        for(Character c : s.toCharArray()){
+            if(table.containsKey(c)){
+                table.put(c,table.get(c)+1);
+            }else{
+                table.put(c,1);
+            }
+        }
+
+        for(Character c : t.toCharArray()){
+            if(table.containsKey(c)){
+                table.put(c,table.get(c)-1);
+            }else{
+                return false;
+            }
+        }
+
+        for(Map.Entry<Character,Integer> e : table.entrySet()){
+            if(e.getValue()!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        String  s = "anagram"; //"rat";
+        String t =  "nngaram" ; //"car";
+
+        ValidAnagram obj = new ValidAnagram();
+        boolean result = obj.checkValidAnagram(s,t);
+        boolean resultWithoutExtraSpace = obj.checkValidAnagramWithoutExtraSpace(s,t);
+
+        if(result){
+            System.out.println("\nProvided input strings are anagram");
+        }else{
+            System.out.println("\nProvided input strings are not anagram");
+        }
+
+        if(resultWithoutExtraSpace){
+            System.out.println("\nProvided input strings are anagram");
+        }else{
+            System.out.println("\nProvided input strings are not anagram");
+        }
+
+    }
+}
