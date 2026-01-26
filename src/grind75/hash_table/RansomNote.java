af57554925e351dd9c848b330b99deb51eda18d2
tinaghosh <ghosh.tina90@gmail.com>
2026-01-26 11:17:12 -0800

    RansomNote

--- src/grind75/hash_table/RansomNote.java
+++ src/grind75/hash_table/RansomNote.java
@@ -1,4 +1,38 @@
 package grind75.hash_table;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RansomNote {
+
+    public boolean ransomNotePossibleFromMagazineUsingArray(String ransomNote,String magazine){
+
+        int[] occurance = new int[26];
+
+        for(Character c:magazine.toCharArray()){
+            occurance[c-'a']++;
+        }
+
+        for(Character c:ransomNote.toCharArray()){
+            if(occurance[c-'a']==0){
+                return false;
+            }
+            occurance[c-'a']--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        String ransomNote = "aa";
+        String magazine = "aab";
+
+        RansomNote obj = new RansomNote();
+        boolean result = obj.ransomNotePossibleFromMagazineUsingArray(ransomNote,magazine);
+        if(result){
+            System.out.println("We can create ransome note from the given magazine");
+        }else{
+            System.out.println("We can not create ransome note from the given magazine");
+        }
+
+    }
 }
