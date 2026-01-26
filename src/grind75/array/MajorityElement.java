164074407b2457c63694694ce02b24e658351786
tinaghosh <ghosh.tina90@gmail.com>
2026-01-26 10:48:27 -0800

    majority Element

--- src/grind75/array/MajorityElement.java
+++ src/grind75/array/MajorityElement.java
@@ -1,4 +1,28 @@
 package grind75.array;
 
 public class MajorityElement {
+    public int findMajorityElement(int[] arr){ //Boyer-Moore Algorithm
+      int maxElement = arr[0];
+      int count = 0;
+      for(int i=0;i<arr.length;i++){
+          if(maxElement==arr[i]){
+              count++;
+          }else{
+             count--;
+             if(count==-1){
+                 maxElement = arr[i];
+                 count = 1;
+             }
+          }
+      }
+      return maxElement;
+    }
+    public static void main(String args[]){
+        int[] nums = {2,2,1,1,1,2,2};
+        MajorityElement obj = new MajorityElement();
+        int result = obj.findMajorityElement(nums);
+        System.out.println("Majority Element in the array is :"+result);
+    }
 }
+
+
