7b832f0ad252791775472ff5f9aaa9317a2356b6
tinaghosh <ghosh.tina90@gmail.com>
2026-01-27 05:50:50 -0800

    Missing Number

--- src/blind75/binary/MissingNumber.java
+++ src/blind75/binary/MissingNumber.java
@@ -1,4 +1,23 @@
 package blind75.binary;
 
 public class MissingNumber {
+
+    public int findMissingNo(int[] nums){
+
+        int n = nums.length;
+        int expectedSum = (n*(n+1))/2;
+        int calculateSum = 0;
+
+        for(int i : nums){
+            calculateSum = calculateSum + i;
+        }
+        return expectedSum-calculateSum;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {3,0,1};
+        MissingNumber obj = new MissingNumber();
+        int result = obj.findMissingNo(nums);
+        System.out.println("Missing No is :"+result);
+    }
 }
