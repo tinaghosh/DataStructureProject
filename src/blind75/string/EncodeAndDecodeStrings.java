ad30b5c30b8626c474141edf42b489cc71ce57ef
tinaghosh <ghosh.tina90@gmail.com>
2026-01-28 07:33:33 -0800

    Encode and decode String

--- src/blind75/string/EncodeAndDecodeStrings.java
+++ src/blind75/string/EncodeAndDecodeStrings.java
@@ -1,4 +1,48 @@
 package blind75.string;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class EncodeAndDecodeStrings {
+
+    public String encodeString(List<String> input){
+      StringBuilder sb = new StringBuilder();
+
+      for(int i=0;i<input.size();i++){
+          int length = input.get(i).length();
+          sb.append(length+"#"+input.get(i));
+      }
+     return sb.toString();
+    }
+
+    public List<String> decodedString(String s){
+       List<String> output = new ArrayList<>();
+       int i=0;
+
+       while(i<s.length()){
+         int j=i;
+         while(s.charAt(j)!='#'){
+             j++;
+         }
+         int wordLength = Integer.parseInt(s.substring(i,j));
+         i = j+1;
+         j = i+wordLength;
+         output.add(s.substring(i,j));
+         i=j;
+       }
+       return output;
+    }
+
+    public static void main(String[] args){
+
+        List<String> input = Arrays.asList("I","love","programming");
+
+        EncodeAndDecodeStrings obj = new EncodeAndDecodeStrings();
+        String encodedString = obj.encodeString(input);
+        System.out.println(encodedString);
+
+        List<String> result = obj.decodedString(encodedString);
+        result.forEach(e->System.out.print(e+" "));
+    }
 }
