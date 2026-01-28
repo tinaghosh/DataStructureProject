469e4d663626643f52d95211b8f1c059ec7d7ff1
tinaghosh <ghosh.tina90@gmail.com>
2026-01-28 11:13:25 -0800

    Reverse bits

--- src/blind75/binary/ReverseBits.java
+++ src/blind75/binary/ReverseBits.java
@@ -1,4 +1,25 @@
 package blind75.binary;
 
 public class ReverseBits {
+
+    public int reverseBits(int n){
+
+        int output = 0;
+
+        for(int i=0;i<32;i++){
+          output = output << 1; //[ shift result left ]
+          int bit = n & 1;
+          output = output | bit;
+          n = n >> 1;  // [ shift n right ]
+        }
+        return output;
+    }
+    public static void main(String[] args){
+
+        int input = 43261596; //binary representation -> 00000010100101000001111010011100;
+
+        ReverseBits obj = new ReverseBits();
+        int result = obj.reverseBits(input);
+        System.out.println("Reverse Bit is :"+result);
+    }
 }
