11ff8e58128316865ace9cbdd2af9265a5783fcc
tinaghosh <ghosh.tina90@gmail.com>
2026-01-29 11:41:33 -0800

    stack

--- src/grind75/stack/EvaluateReversePolishNotation.java
+++ src/grind75/stack/EvaluateReversePolishNotation.java
@@ -37,7 +37,7 @@ public class EvaluateReversePolishNotation {
 
     public static void main(String[] args){
 
-        String arr[] = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+        String[] arr = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
 
         EvaluateReversePolishNotation obj = new EvaluateReversePolishNotation();
         int result = obj.evaluateReversePolishNotation(arr);
