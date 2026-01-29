0c9224a570055c4bc5ff987b0757d6eb5c3e4951
tinaghosh <ghosh.tina90@gmail.com>
2026-01-29 12:25:38 -0800

    StringToInteger

--- src/grind75/string/StringToInteger.java
+++ src/grind75/string/StringToInteger.java
@@ -1,4 +1,52 @@
 package grind75.string;
 
 public class StringToInteger {
+
+    public int convertedInteger(String s){
+
+        int output=0;
+        boolean isNegativeInteger = false;
+        s= s.trim();
+        boolean first = true;
+        for(char c:s.toCharArray()){
+            if((c=='-'||c=='+') && first ){
+               if(c=='-'){
+                   isNegativeInteger=true;
+               }
+               first=false;
+               continue;
+            }else{
+               if(isDigit(c)){
+                   if (output > Integer.MAX_VALUE / 10 || (output == Integer.MAX_VALUE / 10 && c-'0' > 7)) {
+                       return  isNegativeInteger == false ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                   }
+                   output = output*10+(c-'0');
+               }else{
+                   break;
+               }
+            }
+            first=false;
+        }
+        if(isNegativeInteger){
+            output= -1 * output;
+        }
+       return output;
+    }
+
+    public boolean isDigit(char c){
+        int i=c-'0';
+        if(0<=i && i<=9){
+            return true;
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args){
+        String s = "+-12";//""0-1";
+
+        StringToInteger obj = new StringToInteger();
+        int result = obj.convertedInteger(s);
+        System.out.println("Converted Integer from String is :"+result);
+    }
 }
