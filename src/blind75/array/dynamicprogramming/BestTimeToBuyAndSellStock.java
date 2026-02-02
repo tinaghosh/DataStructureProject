7b22403edb6b00c04cdb8a916c59c7db77efa3bb
tinaghosh <ghosh.tina90@gmail.com>
2026-02-01 19:09:02 -0800

    Greedy Techniques

--- src/blind75/array/dynamicprogramming/BestTimeToBuyAndSellStock.java
+++ src/blind75/array/dynamicprogramming/BestTimeToBuyAndSellStock.java
@@ -1,4 +1,21 @@
 package blind75.array.dynamicprogramming;
 
 public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices){
+       int maxProfit = 0;
+       int minPrice = prices[0];
+       for(int i=1;i<prices.length;i++){
+           maxProfit = Math.max(maxProfit,prices[i] - minPrice);
+           minPrice = Math.min(minPrice,prices[i]);
+       }
+       return maxProfit;
+    }
+
+    public static void main(String[] args){
+        int[] prices = {7,1,5,3,6,4};
+        BestTimeToBuyAndSellStock obj = new BestTimeToBuyAndSellStock();
+        int result = obj.maxProfit(prices);
+        System.out.println("Max Profit can be -> "+result);
+    }
 }
--- src/blind75/dynamic_programming/JumpGame.java
+++ src/blind75/dynamic_programming/JumpGame.java
@@ -1,4 +1,30 @@
 package blind75.dynamic_programming;
 
 public class JumpGame {
+
+    public boolean possibleReachingLastIndex(int[] nums){
+
+        int goal = nums.length-1;
+
+        for(int i=nums.length-2;i>=0;i--){
+            if((nums[i]+i) >= goal){
+                goal =i;
+            }
+        }
+        if(goal == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        int[]  nums = {3,2,1,0,4};//{2,3,1,1,4};
+        JumpGame  obj = new JumpGame();
+        boolean result = obj.possibleReachingLastIndex(nums);
+        if(result){
+            System.out.println("Yes It's possible to reach last index");
+        }else{
+            System.out.println("No It's not possible to reach last index");
+        }
+    }
 }
