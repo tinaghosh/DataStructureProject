5ea0de4b891978e66ec529066592d6f0480bea65
tinaghosh <ghosh.tina90@gmail.com>
2026-02-02 23:24:20 -0800

    stacks

--- src/grind75/stack/ImplementQueueUsingStacks.java
+++ src/grind75/stack/ImplementQueueUsingStacks.java
@@ -1,4 +1,76 @@
 package grind75.stack;
 
+
+import java.util.Stack;
+
 public class ImplementQueueUsingStacks {
+
+     Stack<Integer> s1 = null;
+     Stack<Integer> s2 = null;
+
+     public ImplementQueueUsingStacks(){
+          s1 = new Stack();
+          s2 = new Stack();
+     }
+     public void push(int[] input){
+         for(int i=0;i<input.length;i++){
+             s1.push(input[i]);
+         }
+     }
+
+     public int peek(){
+         if(!s2.isEmpty()){
+             return s2.peek();
+         }else {
+             while (!s1.isEmpty()) {
+                 int popValue = s1.pop();
+                 s2.push(popValue);
+             }
+             return s2.peek();
+         }
+     }
+
+     public int pop(){
+         if(!s2.isEmpty()){
+             return s2.pop();
+         }else{
+             while(!s1.isEmpty()) {
+                 int popValue = s1.pop();
+                 s2.push(popValue);
+             }
+             return s2.pop();
+         }
+     }
+
+     public boolean isEmpty(){
+         if(Math.max(s1.size(), s2.size())==0){
+             return true;
+         }
+         return false;
+     }
+
+
+    public static void main(String[] args){
+        String[] operations = {"MyQueue","push","push","push","push","pop","push","pop","pop","pop","pop"};
+        int[][] inputsForOperations = {{},{1},{2},{3},{4},{},{5},{},{},{},{}};
+        ImplementQueueUsingStacks obj = null;
+
+        for(int i =0;i<operations.length;i++) {
+
+            if(operations[i].equals("MyQueue")){
+                obj = new ImplementQueueUsingStacks();
+            }else if(operations[i].equals("push")){
+                obj.push(inputsForOperations[i]);
+            }else if(operations[i].equals("peek")){
+                int result = obj.peek();
+                System.out.println(result);
+            }else if(operations[i].equals("pop")){
+                int result = obj.pop();
+                System.out.println(result);
+            }else if(operations[i].equals("empty")){
+                boolean result = obj.isEmpty();
+                System.out.println(result);
+            }
+        }
+    }
 }
--- src/grind75/stack/LargestRectangleInHistogram.java
+++ src/grind75/stack/LargestRectangleInHistogram.java
@@ -1,4 +1,53 @@
 package grind75.stack;
 
+import java.util.Stack;
+
 public class LargestRectangleInHistogram {
+
+    public int findMaxAreaInHistogram(int[] heights) {
+
+        int maxArea = 0;
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(0, heights[0]));
+
+        for (int i = 1; i < heights.length; i++) {
+            Pair prev = stack.peek();
+            if (prev.height < heights[i]) {
+                stack.push(new Pair(i, heights[i]));
+            } else {
+                Pair poppedPair = null;
+                while (!stack.isEmpty() && stack.peek().height >= heights[i]) {
+                    poppedPair = stack.pop();
+                    maxArea = Math.max(maxArea, (poppedPair.height * (i - poppedPair.startIndex)));
+                }
+                if(poppedPair!=null)
+                    stack.push(new Pair(poppedPair.startIndex,heights[i]));
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            Pair p = stack.pop();
+            maxArea = Math.max(maxArea, (p.height * (heights.length - p.startIndex)));
+        }
+        return maxArea;
+    }
+
+    class Pair {
+        int startIndex = 0;
+        int height = 0;
+
+        Pair(int startIndex, int height) {
+            this.startIndex = startIndex;
+            this.height = height;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] heights = {3,6,5,7,4,8,1,0};//{2, 1, 5, 6, 2, 3};
+        LargestRectangleInHistogram obj = new LargestRectangleInHistogram();
+        int result = obj.findMaxAreaInHistogram(heights);
+        System.out.println("Largest Rectangle Area in the Histogram is -> " + result);
+    }
 }
+
+
