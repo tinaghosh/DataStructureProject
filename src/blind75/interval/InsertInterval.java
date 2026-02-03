3695f4506b8b5bea530eb4fbd52cf1f8cbb3e775
tinaghosh <ghosh.tina90@gmail.com>
2026-02-02 23:27:00 -0800

    insert intervals

--- src/blind75/interval/InsertInterval.java
+++ src/blind75/interval/InsertInterval.java
@@ -1,8 +1,51 @@
 package blind75.interval;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InsertInterval {
 
-    public static void main(String[] args){
+    public int[][] insertAndMergeInterval(int[][] intervals, int[] newInterval) {
 
+        List<int[]> output = new ArrayList<>();
+        int indicator = 0;
+
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] < newInterval[0]) {
+                output.add(intervals[i]);
+                indicator ++;
+            }
+        }
+        if (output.size() >= 1) {
+            int[] prevInterval = output.getLast();
+            if (prevInterval[1] >= newInterval[0]) {
+                output.getLast()[0] = Math.min(output.getLast()[0], newInterval[0]);
+                output.getLast()[1] = Math.max(output.getLast()[1], newInterval[1]);
+            }else{
+                output.add(newInterval);
+            }
+        } else {
+            output.add(newInterval);
+        }
+        for (int i = indicator; i < intervals.length; i++) {
+            if (output.getLast()[1] >= intervals[i][0]) {
+                output.getLast()[0] = Math.min(output.getLast()[0], intervals[i][0]);
+                output.getLast()[1] = Math.max(output.getLast()[1], intervals[i][1]);
+            } else {
+                output.add(intervals[i]);
+            }
+        }
+        return output.toArray(int[][]::new);
+    }
+
+    public static void main(String[] args) {
+
+        int[][] intervals = {{1,5}};//{{1, 2}, {3, 4}, {5, 8}, {9, 15}};// {{1,2},{3,5},{6,7},{8,10},{12,16}};
+        int[] newInterval = {6,8};//{2, 5};// {4,8};
+
+        InsertInterval obj = new InsertInterval();
+        int[][] result = obj.insertAndMergeInterval(intervals, newInterval);
+        Arrays.stream(result).forEach(e -> System.out.print("[" + e[0] + "," + e[1] + "]"));
     }
 }
