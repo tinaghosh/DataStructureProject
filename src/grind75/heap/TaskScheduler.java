08e444ca3f47a9788efd55106ae7bbe929df886c
tinaghosh <ghosh.tina90@gmail.com>
2026-02-06 12:05:10 -0800

    TaskScheduler

--- src/grind75/heap/TaskScheduler.java
+++ src/grind75/heap/TaskScheduler.java
@@ -1,4 +1,49 @@
 package grind75.heap;
 
+import java.util.*;
+
 public class TaskScheduler {
+
+    public int minIntervalsRequired(String[] tasks, int n) {
+        int totalInterval = 0;
+        Map<String, Integer> frequencyMap = new HashMap<>();
+
+        for (String taskName : tasks) {
+            frequencyMap.put(taskName, (frequencyMap.getOrDefault(taskName, 0) + 1));
+        }
+
+        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String s : frequencyMap.keySet()) {
+            maxHeap.add(frequencyMap.get(s));
+        }
+
+        while (!maxHeap.isEmpty()) {
+            int iterationLength = n + 1;
+            List<Integer> store = new ArrayList<>();
+            int taskCount = 0;
+            while (iterationLength > 0 && !maxHeap.isEmpty()) {
+                int currentFreq = maxHeap.poll();
+                if (currentFreq - 1 > 0) {
+                    store.add(currentFreq - 1);
+                }
+                iterationLength--;
+                taskCount++;
+            }
+            store.forEach(e -> {
+                maxHeap.add(e);
+            });
+            totalInterval = maxHeap.isEmpty() ? (totalInterval + taskCount) : (totalInterval + (n + 1));
+        }
+        return totalInterval;
+    }
+
+    public static void main(String[] args) {
+        String[] tasks = {"A", "A", "A", "B", "B", "B"};
+        int n = 2;
+
+        TaskScheduler obj = new TaskScheduler();
+        int result = obj.minIntervalsRequired(tasks, n);
+        System.out.print("Minimum number of CPU intervals required to complete all tasks -> " + result);
+    }
 }
