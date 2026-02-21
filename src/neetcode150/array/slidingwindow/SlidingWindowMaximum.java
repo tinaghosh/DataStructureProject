14cce855a5a3678e8143aae84e19dec20579aa2f
tinaghosh <ghosh.tina90@gmail.com>
2026-02-21 07:47:01 -0800

    sort color

--- src/neetcode150/array/slidingwindow/SlidingWindowMaximum.java
+++ src/neetcode150/array/slidingwindow/SlidingWindowMaximum.java
@@ -31,7 +31,7 @@ public class SlidingWindowMaximum {
         }
         return result;
     }
-    public static void main(String args[]){
+    public static void main(String[] args){
         int[] nums = {1,3,-1,-3,5,3,6,7};
         int k = 3;
 
