031c2171c2acc6ae7e5838de45ba2311224dcc0e
tinaghosh <ghosh.tina90@gmail.com>
2026-02-24 06:57:30 -0800

    MergeTwoSortedLists

--- src/blind75/linkedlist/MergeTwoSortedLists.java
+++ src/blind75/linkedlist/MergeTwoSortedLists.java
@@ -3,80 +3,58 @@ package blind75.linkedlist;
 
 public class MergeTwoSortedLists {
 
-    public LinkNode mergeTwoLists(LinkNode list1,LinkNode list2){
+    public LinkNode mergeTwoLists(LinkNode list1, LinkNode list2) {
 
         LinkNode mergeList = null;
 
-        if(list1!=null && list2==null){
+        if (list1 != null && list2 == null) {
             return list1;
-        }else if(list1==null && list2!=null){
+        }
+        if (list1 == null && list2 != null) {
             return list2;
-        }else {
-
-            while (list1 != null && list2 != null) {
-                if (list1.data < list2.data) {
-                    if (mergeList == null) {
-                        mergeList = new LinkNode(list1.data);
-                    } else {
-                       LinkNode currentNode = mergeList;
-                       while(currentNode.nextNode!=null) {
-                           currentNode = currentNode.nextNode;
-                       }
-                        currentNode.nextNode = new LinkNode(list1.data);
-                    }
-                    list1 = list1.nextNode;
-
-                } else if (list1.data > list2.data) {
-                    if (mergeList == null) {
-                        mergeList = new LinkNode(list2.data);
-                    } else {
-                        LinkNode currentNode = mergeList;
-                        while(currentNode.nextNode!=null){
-                            currentNode = currentNode.nextNode;
-                        }
-                        currentNode.nextNode = new LinkNode(list2.data);
-                    }
-                    list2 = list2.nextNode;
-
-                } else {
-                    if (mergeList == null) {
-                        mergeList = new LinkNode(list1.data);
-                    } else {
-                        LinkNode currentNode = mergeList;
-                        while(currentNode.nextNode!=null){
-                            currentNode = currentNode.nextNode;
-                        }
-                        currentNode.nextNode = new LinkNode(list1.data);
-                    }
-                    LinkNode currentNode = mergeList;
-                    while(currentNode.nextNode!=null){
-                        currentNode = currentNode.nextNode;
-                    }
-                    currentNode.nextNode = new LinkNode(list2.data);
-                    list1 = list1.nextNode;
-                    list2 = list2.nextNode;
-                }
-            }
-            if(list1!=null) {
-                LinkNode currentNode = mergeList ;
-                while(currentNode.nextNode!=null){
-                    currentNode = currentNode.nextNode;
-                }
-                currentNode.nextNode = list1;
-            }
-            if(list2!=null) {
-                LinkNode currentNode = mergeList ;
-                while(currentNode.nextNode!=null){
-                    currentNode = currentNode.nextNode;
-                }
-                currentNode.nextNode = list2;
+        }
+        while (list1 != null && list2 != null) {
+            if (list1.data < list2.data) {
+                mergeList = insertAtEnd(mergeList, list1.data);
+                list1 = list1.nextNode;
+            } else if (list1.data > list2.data) {
+                mergeList = insertAtEnd(mergeList, list2.data);
+                list2 = list2.nextNode;
+            } else {
+                mergeList = insertAtEnd(mergeList, list1.data);
+                mergeList = insertAtEnd(mergeList, list2.data);
+                list1 = list1.nextNode;
+                list2 = list2.nextNode;
             }
         }
-
+        while (list1 != null) {
+            mergeList = insertAtEnd(mergeList, list1.data);
+            list1 = list1.nextNode;
+        }
+        while (list2 != null) {
+            mergeList = insertAtEnd(mergeList, list2.data);
+            list2 = list2.nextNode;
+        }
         return mergeList;
     }
 
-    public static void printLinkedList(LinkNode head){
+    public LinkNode insertAtEnd(LinkNode head, int data) {
+
+        LinkNode current = head;
+        LinkNode newNode = new LinkNode(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (current.nextNode != null) {
+                current = current.nextNode;
+            }
+            current.nextNode = newNode;
+        }
+        return head;
+    }
+
+    public static void printLinkedList(LinkNode head) {
 
         LinkNode currentNode = head;
         while (currentNode != null) {
@@ -88,10 +66,10 @@ public class MergeTwoSortedLists {
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int[] listArray1 = {1,2,4,5};
-        int[] listArray2 = {1,3,4,6,7};
+        int[] listArray1 = {1, 2, 4, 5};
+        int[] listArray2 = {1, 3, 4, 6, 7};
 
         LinkNode list1 = new LinkNode(listArray1[0]);
         list1.nextNode = new LinkNode(listArray1[1]);
@@ -105,7 +83,7 @@ public class MergeTwoSortedLists {
         list2.nextNode.nextNode.nextNode.nextNode = new LinkNode(listArray2[4]);
 
         MergeTwoSortedLists obj = new MergeTwoSortedLists();
-        LinkNode result = obj.mergeTwoLists(list1,list2);
+        LinkNode result = obj.mergeTwoLists(list1, list2);
         printLinkedList(result);
 
     }
