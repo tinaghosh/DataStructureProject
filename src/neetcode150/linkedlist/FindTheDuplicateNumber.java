2b783e2f79dd9218c578974a01a035d09b75fc7f
tinaghosh <ghosh.tina90@gmail.com>
2026-07-22 15:48:46 -0700

    FindTheDuplicateNumber

---
+++ src/neetcode150/linkedlist/FindTheDuplicateNumber.java
@@ -0,0 +1,30 @@
+package neetcode150.linkedlist;
+
+public class FindTheDuplicateNumber {
+
+    public int findDuplicate(int[] nums) {
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+
+        slow = nums[0];
+
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {3,1,3,4,2};
+        FindTheDuplicateNumber obj = new FindTheDuplicateNumber();
+        int result = obj.findDuplicate(nums);
+        System.out.println(result);
+    }
+}
